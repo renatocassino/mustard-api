8ae3c13eea269688673745af85131bf7c111ef0b
Renato Cassino <renatocassino@gmail.com>
2019-11-02 15:45:19 -0300

    Create migrations

--- go.mod
+++ go.mod
@@ -3,17 +3,27 @@ module api
 go 1.13
 
 require (
+	cloud.google.com/go v0.36.0 // indirect
+	github.com/cockroachdb/cockroach-go v0.0.0-20190916165215-ad57a61cc915 // indirect
 	github.com/gobuffalo/buffalo v0.15.0
 	github.com/gobuffalo/buffalo-pop v1.23.1
 	github.com/gobuffalo/envy v1.7.1
+	github.com/gobuffalo/fizz v1.9.5 // indirect
+	github.com/gobuffalo/gogen v0.2.0 // indirect
 	github.com/gobuffalo/mw-contenttype v0.0.0-20190129203934-2554e742333b
+	github.com/gobuffalo/mw-csrf v0.0.0-20190129204204-25460a055517 // indirect
 	github.com/gobuffalo/mw-forcessl v0.0.0-20180802152810-73921ae7a130
 	github.com/gobuffalo/mw-paramlogger v0.0.0-20190129202837-395da1998525
 	github.com/gobuffalo/packr/v2 v2.7.1
 	github.com/gobuffalo/pop v4.12.2+incompatible
 	github.com/gobuffalo/suite v2.8.2+incompatible
+	github.com/gobuffalo/validate v2.0.3+incompatible
 	github.com/gobuffalo/x v0.0.0-20190224155809-6bb134105960
+	github.com/gofrs/uuid v3.2.0+incompatible
+	github.com/jackc/pgconn v1.1.0 // indirect
+	github.com/jackc/pgx v3.5.0+incompatible // indirect
 	github.com/markbates/grift v1.1.0
 	github.com/rs/cors v1.7.0
+	github.com/spf13/afero v1.2.1 // indirect
 	github.com/unrolled/secure v0.0.0-20190103195806-76e6d4e9b90c
 )
---
+++ models/Lyric.go
@@ -0,0 +1,47 @@
+package models
+
+import (
+	"encoding/json"
+	"github.com/gobuffalo/pop"
+	"github.com/gobuffalo/validate"
+	"github.com/gofrs/uuid"
+	"time"
+)
+type Lyric struct {
+    ID uuid.UUID `json:"id" db:"id"`
+    CreatedAt time.Time `json:"created_at" db:"created_at"`
+    UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+}
+
+// String is not required by pop and may be deleted
+func (l Lyric) String() string {
+	jl, _ := json.Marshal(l)
+	return string(jl)
+}
+
+// Lyrics is not required by pop and may be deleted
+type Lyrics []Lyric
+
+// String is not required by pop and may be deleted
+func (l Lyrics) String() string {
+	jl, _ := json.Marshal(l)
+	return string(jl)
+}
+
+// Validate gets run every time you call a "pop.Validate*" (pop.ValidateAndSave, pop.ValidateAndCreate, pop.ValidateAndUpdate) method.
+// This method is not required and may be deleted.
+func (l *Lyric) Validate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// ValidateCreate gets run every time you call "pop.ValidateAndCreate" method.
+// This method is not required and may be deleted.
+func (l *Lyric) ValidateCreate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// ValidateUpdate gets run every time you call "pop.ValidateAndUpdate" method.
+// This method is not required and may be deleted.
+func (l *Lyric) ValidateUpdate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
